a715742caf44d9c57d7a66e9317260f9ecad6f36
kasoya <shuyang_x@hotmail.com>
2016-09-13 13:24:25 +0800

    init commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+[from] acmore.cc
+
+[purpose] 堆 java 码量。
+
+[note] Main0 表示 1000，Main1 表示 1001，依次类推
---
+++ src/Main0.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main0{
+  public static void main(String args[]){
+    Scanner in = new Scanner(System.in);
+    int a, b;
+    while (in.hasNext()){
+      a = in.nextInt();
+      b = in.nextInt();
+      System.out.println(a + b);
+    }
+  }
+}
